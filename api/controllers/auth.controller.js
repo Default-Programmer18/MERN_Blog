3707c803464c8cac911e85d65fc4472e6909750f
sanghita <sanghita@gmail.com>
2024-03-12 19:45:31 +0530

    sign-in api route created in server

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User=require("../models/user.model.js")
 const bcryptjs =require("bcryptjs")
 const { errorHandler } = require("../utils/errorHandler.js")
+const jwt=require("jsonwebtoken")
 
 //next is the middleware
 const signup=async(req,res,next)=>{
@@ -29,7 +30,50 @@ const signup=async(req,res,next)=>{
     
 }
 
+///////////////////////////signin/////////////////////////////////////
+const signin = async(req, res, next)=>
+{
+    const {username,password,email}=req.body
+    if(!password   || password===""||!email || email==="" )
+        next(errorHandler(400,"All fields are required..."))
+    
+    try{    
+    const validUser=await User.findOne({email});
+     if(!validUser)
+        return  next(errorHandler(404,"User not found"));
+    const validPassword = bcryptjs.compareSync(password,validUser.password);
+    if(!validPassword) 
+        return next(errorHandler("400","Invalid password"));
+    const token=jwt.sign(
+        {id:validUser._id, },
+        process.env.JWT_SECRET,
+        // {
+        //     expiresIn:3600
+        // }//if not given expires as user closes the browswer
+
+    );
+        //removes password and add aother data to rest
+        //assigning password to pass
+        const{ password: pass ,...rest}=validUser._doc;
+        //validUser._doc: Assuming validUser is an object, 
+        //the _doc property is often used in some MongoDB libraries like Mongoose to get the raw document data.
+    res.status(200).cookie("access_token",token,{
+        httpOnly:true
+    }).json(rest);
+
+
+    }
+    catch(error)
+    {
+        next(error);
+    }
+
+
+}
+///////////////////////////signin/////////////////////////////////////
+
 
 module.exports={
     signup,
+    signin,
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { signup } = require('../controllers/auth.controller');
+const { signup,signin } = require('../controllers/auth.controller');
 
 
 
 const router =express.Router();
 
-router.post('/sign-up',signup); 
+router.post('/sign-up',signup);
+router.post('/sign-in',signin); 
+
 
 module.exports=router
